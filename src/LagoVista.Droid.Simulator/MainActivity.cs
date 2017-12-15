5a5c36c358851269d54aa310c37299d3c2869982
Kevin D. Wolf <kevinw@software-logistics.com>
2017-12-15 18:59:13 -0500

    working iOS/Android on sim

--- src/LagoVista.Droid.Simulator/MainActivity.cs
+++ src/LagoVista.Droid.Simulator/MainActivity.cs
@@ -1,13 +1,11 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
-using LagoVista.Client.Core.Auth;
-using LagoVista.Core.IOC;
 using static LagoVista.Droid.Simulator.Resource;
 
 namespace LagoVista.Simulator.Droid
 {
-    [Activity(Label = "IoT Simulator", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "IoT Simulator", Icon = "@drawable/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         public const string MOBILE_CENTER_KEY = "1211d9f6-eed2-44a1-bfac-e7b080e000c1";
@@ -19,8 +17,6 @@ namespace LagoVista.Simulator.Droid
 
             //https://play.google.com/apps/publish/?dev_acc=12258406958683843289
             LagoVista.XPlat.Droid.Startup.Init(BaseContext, MOBILE_CENTER_KEY);
-
-            //SLWIOC.Register<ISecureStorage, SecureStorage>();
 
             base.OnCreate(bundle);
 
